5807f22ef18fcbccafbbbca3cc8320a4f2e35014
Jigsaw <j1g5aw@foxmail.com>
2021-12-27 23:00:05 +0800

    :art: Only one matcher

--- OlivOS/matcher.py
+++ OlivOS/matcher.py
@@ -1,16 +1,13 @@
 from nonebot import get_driver
 from nonebot.adapters import Bot, Event
-from nonebot.plugin import on_message, on_metaevent, on_notice, on_request
+from nonebot.plugin import on
 
 from OlivOS.middlewares import Proc
 from OlivOS.middlewares.onebot import OlivOSEvent
 from OlivOS.plugin import get_loaded_plugins, load_plugins
 
 driver = get_driver()
-message_matcher = on_message()
-notice_matcher = on_notice()
-request_matcher = on_request()
-meta_matcher = on_metaevent()
+matcher = on()
 
 
 @driver.on_startup
@@ -26,28 +23,7 @@ async def _(bot: Bot):
         p.route.init_after(None, Proc())
 
 
-@message_matcher.handle()
-async def _(bot: Bot, event: Event):
-    for p in get_loaded_plugins():
-        ovo_event = OlivOSEvent(bot, event)
-        getattr(p.route, ovo_event.plugin_info["func_type"])(ovo_event, Proc())
-
-
-@notice_matcher.handle()
-async def _(bot: Bot, event: Event):
-    for p in get_loaded_plugins():
-        ovo_event = OlivOSEvent(bot, event)
-        getattr(p.route, ovo_event.plugin_info["func_type"])(ovo_event, Proc())
-
-
-@request_matcher.handle()
-async def _(bot: Bot, event: Event):
-    for p in get_loaded_plugins():
-        ovo_event = OlivOSEvent(bot, event)
-        getattr(p.route, ovo_event.plugin_info["func_type"])(ovo_event, Proc())
-
-
-@meta_matcher.handle()
+@matcher.handle()
 async def _(bot: Bot, event: Event):
     for p in get_loaded_plugins():
         ovo_event = OlivOSEvent(bot, event)
